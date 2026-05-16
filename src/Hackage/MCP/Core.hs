00353996d7d350b97749224a120e7fec489e4e3e
Tushar Adhatrao <tusharadhatrao@gmail.com>
2026-05-16 10:03:01 +0000

    added test case for checking server version

--- src/Hackage/MCP/Core.hs
+++ src/Hackage/MCP/Core.hs
@@ -1,4 +1,4 @@
-module Hackage.MCP.Core (runApp) where
+module Hackage.MCP.Core (runApp, mcpServerInfo) where
 
 import Data.Text (Text)
 import Hackage.MCP.Tool (toolHandlers)
@@ -7,14 +7,16 @@ import MCP.Server
 customInstructions :: Text
 customInstructions = "Use tools to search Hoogle, list package modules, and fetch concise Markdown docs for Haskell modules from Hackage. When passing module names to get_module_docs, convert them to hyphen-casing: for example, Data.List becomes Data-List."
 
+mcpServerInfo :: McpServerInfo
+mcpServerInfo =
+    McpServerInfo
+        { serverName = "hackage-doc"
+        , serverVersion = "0.0.2.0"
+        , serverInstructions = customInstructions
+        }
+
 runApp :: IO ()
 runApp = do
-    let mcpServerInfo =
-            McpServerInfo
-                { serverName = "hackage-doc"
-                , serverVersion = "0.1.0"
-                , serverInstructions = customInstructions
-                }
     let mcpServerHandlers =
             McpServerHandlers
                 { prompts = Nothing
